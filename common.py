36c9e87023e5fb77a15b1d1893ae94d92cb3dfee
Aryan <aryan.rathore@dianapps.com0>
2022-09-23 15:47:16 +0530

    Common python methods

---
+++ common.py
@@ -0,0 +1,112 @@
+# print("Hello Dians ")
+# x = input("enter the number")
+# print ("Your number is :" + x)
+
+
+
+# Calculator programe using if else
+
+# x= input('enter the first number')
+# y=input ('enter the operator')
+# z= input("enter the second number")
+
+# x=int(x)
+# z=int(z)
+
+# if y=='+':
+#     print(x+z)
+
+# elif y=='-':
+#     print(x-z)
+
+# elif y=='*':
+#     print(x*z)
+
+# elif y=='/':
+#     print(x/z)
+
+# elif y=='%':
+#     print(x%z)
+# else:
+#     print("invalid operator")
+
+
+# loops 
+# while loop ( star print)
+
+# i=1
+# while i<=10:
+#     print(i*"*")
+#     i=i+1
+
+# for loop print 1-100
+
+# for i in range(100):
+#     print(i)
+
+# list 
+
+
+# salary =[1,2,3,4,5,6,7,8]
+
+
+# from pickle import APPEND
+
+
+
+
+# salary.append(9) 
+# print(salary)
+
+# print(salary[3:7])
+
+# for value in salary:
+#     print(value)
+
+# # print(len(salary))
+# salary.clear()
+# print(salary)
+
+#break statement
+
+# for salary in salary :
+#     if (salary==6):
+#         break;
+#     print(salary)
+
+#continue statement
+
+# for salary in salary :
+#     if (salary==6):
+#         continue;
+#     print(salary)
+
+
+# tuple
+
+# tuple = ("a","a","a","a","a","s","d","f","g",'h',"v","c")
+
+# print(tuple.count('a'))
+# print(tuple.index('d'))
+
+# sets 
+
+# marks ={12,34,445,66,76,43}
+
+#dictionary
+
+# sal ={"aryan":500 , "nidhi": 490 , "gaurav":350 , "srb":910}
+# print(sal["gaurav"])
+# print(sal)
+# sal["vaibhav"]=12003;
+# print(sal)
+
+# sal["aryan"]=551561;
+# print(sal)
+
+#function
+
+# def print_sum(first ,second=5):
+#     print(first+second)
+
+# print_sum(5)
