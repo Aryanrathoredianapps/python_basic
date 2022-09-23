a164bc51877865b656d8246234eb719d2641432c
Aryan <aryan.rathore@dianapps.com0>
2022-09-23 17:04:45 +0530

    second commit

---
+++ list.py
@@ -0,0 +1,36 @@
+# list methods 
+
+wap = ['aryan','p',2,13,555,"dian"]
+wap.insert(6,'vaibhav')
+print(wap)
+
+wap.append(55)
+print(wap)
+
+wap.pop()
+print(wap)
+
+wap.reverse()
+print(wap)
+
+wap.append("aryan")
+print(wap)
+
+wap.count("aryan"
+)
+
+wap2 =["nidhi","srrb"]
+
+wap.extend(wap2)
+print(wap)
+
+#TypeError: '<' not supported between instances of 'int' and 'str'
+# wap.sort()
+# print(wap)
+
+wap.remove("aryan")
+print(wap)
+
+
+wap.clear()
+print(wap)
---
+++ tuple.py
@@ -0,0 +1,12 @@
+wap =(2,33,42,23,23,23,'aryan','vaibhav')
+print(wap)
+
+print(len(wap))
+# TypeError: '<' not supported between instances of 'int' and 'str'
+# print(max(wap))
+
+wap.index(23)
+
+wap.count(23)
+
+
