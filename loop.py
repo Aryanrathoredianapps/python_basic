18f8f3e48a8ea93d96f560173189a79ce6ce1833
Aryan <aryan.rathore@dianapps.com0>
2022-09-26 11:45:20 +0530

    loops file added

---
+++ loop.py
@@ -0,0 +1,25 @@
+
+i=1
+while i<=10:
+    print(i)
+    if i ==6:
+        break;
+    i=i+1
+
+
+for i in range (20):
+    print (i)
+
+
+
+x= ['p','l','r','g','t']
+for items in x:
+    if items=='r':
+     continue
+    print(items)
+
+
+
+
+
+#use pass if in case u have no containt in for loop
